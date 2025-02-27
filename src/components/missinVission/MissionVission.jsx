48ef8c4293e059d76d4c485a778f0b8283d3752a
Feizo <felixkebaki@gmail.com>
2025-02-27 14:37:27 +0300

    Update mission and vission

--- src/components/missinVission/MissionVission.jsx
+++ src/components/missinVission/MissionVission.jsx
@@ -16,9 +16,7 @@ export function MissionVission() {
           <div className="EachDivMainContentAtMissionVission">
             <p id="VissionMissionTitle" className="medium">Mission <FontAwesomeIcon icon={faRocket} id="MissionVissionIcon"/></p>
             <p id="VissionMissionDesc" className="regular">
-              To uplift vulnerable children in rural communities by providing
-              them with essential support in education, food, clothing, shelter,
-              and mental recovery.
+            To uplift and empower vulnerable children by providing access to education, basic needs, and mental well-being support, ensuring they have a strong foundation for a brighter future.
             </p>
           </div>
         </div>
@@ -27,7 +25,7 @@ export function MissionVission() {
           <div className="EachDivMainContentAtMissionVission">
             <p id="VissionMissionTitle" className="medium">Vission <FontAwesomeIcon icon={faEye} id="MissionVissionIcon"/></p>
             <p id="VissionMissionDesc" className="regular">
-            To create a thriving future where every vulnerable child in Africa’s rural communities has access to education, nourishment, shelter and emotional well-being, empowering them to reach their full potential and break the cycle of poverty.
+            To make Africa a mother of all opportunities
             </p>
           </div>
         </div>
