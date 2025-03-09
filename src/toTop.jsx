0e76ed66048c3dcb0c191c5068fc77112fc389a4
Feizo <felixkebaki@gmail.com>
2025-03-09 08:54:33 +0300

    ToTop

--- src/toTop.jsx
+++ src/toTop.jsx
@@ -1,49 +1,51 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowUp} from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
-import './index.css'
+import "./index.css";
 
 export function ToTop() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    const HandleScroll=()=>{
-        let scrollToTop=document.querySelector(".ToTopDiv")
-        if(window.scrollY>300){
-          scrollToTop.classList.add("ToTopDivShow");
-        }
-        else{
-          scrollToTop.classList.remove("ToTopDivShow");
-        }
-      }
-      
+  const HandleScroll = () => {
+    let scrollToTop = document.querySelector(".ToTopDiv");
+    if (window.scrollY > 300) {
+      scrollToTop.classList.add("ToTopDivShow");
+    } else {
+      scrollToTop.classList.remove("ToTopDivShow");
+    }
+  };
 
-    const HandleToTop = () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      });
-    };
-  
-  
+  const HandleToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
-    window.addEventListener("scroll",HandleScroll)
+    window.addEventListener("scroll", HandleScroll);
 
-    return()=>{
-      window.removeEventListener("scroll",HandleScroll)
-    }
-  
+    return () => {
+      window.removeEventListener("scroll", HandleScroll);
+    };
   }, [pathname]);
   return (
-    <div className="ToTopDiv">
+    <section className="ToTopMainSec">
+      <a href={import.meta.env.VITE_WHATSAPP_LINK} className="WhatsAppIconLink">
+        <FontAwesomeIcon icon={faWhatsapp} id="WhatsAppIcon"/>
+      </a>
+      <div className="ToTopDiv">
         <FontAwesomeIcon
           icon={faArrowUp}
           className="ToTop"
           onClick={HandleToTop}
         />
-    </div>
+      </div>
+    </section>
   );
 }
